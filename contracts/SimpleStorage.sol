8c228db19f993316f7715f1d4bd4295e380bc292
saya1k <hanna-dev@protonmail.com>
2022-10-09 16:28:58 -0700

    simplestore

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
+
+contract SimpleStorage {
+    //variable types: bool, uint, int, address, bytes, string
+    uint256 favoriteNumber;
+    // struct
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    //array
+    People[] public people;
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+    //view: reads state without spending gas unless called within another function (does not modify state)
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    }
+    //add people to the array
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+    }
+}
